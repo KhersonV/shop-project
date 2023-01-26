2547fd1ed1c4e26d15805a38d7a23e5d90849f2e
KhersonV <khersonv110990@gmail.com>
2023-01-25 22:00:40 -0600

    add App

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
+const Title = () => {
+    return <h1>Title</h1>
+}
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(<React.StrictMode></React.StrictMode>);
+const Content = () => {
+    return (
+        <React.Fragment>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor,
+                ex dicta voluptatum saepe, natus sit quasi pariatur nulla, nihil
+                quisquam quibusdam optio sequi. Deserunt pariatur reiciendis
+                fugit odit provident quas.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor,
+                ex dicta voluptatum saepe, natus sit quasi pariatur nulla, nihil
+                quisquam quibusdam optio sequi. Deserunt pariatur reiciendis
+                fugit odit provident quas.
+            </p>
+        </React.Fragment>
+    )
+}
+
+function App() {
+    return (
+        <>
+            <Title />
+            <Content />
+        </>
+    )
+}
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
