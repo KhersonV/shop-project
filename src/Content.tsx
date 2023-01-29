42f2136654b8051ecc7885364edeb8f2859d1b2f
KhersonV <khersonv110990@gmail.com>
2023-01-29 14:05:35 -0600

    add Class to p

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,18 +1,16 @@
+import "./Content.css"
+
 type Props = {
     text: string
     text2: string
     year: number
 }
 const Content = (props: Props) => {
-  let data = true
+  
     return (
         <>
             <p
-                style={{
-                    color: data ? 'yellow' : 'White',
-                    backgroundColor: 'green',
-                    padding: `15px`,
-                }}
+               className="content"
             >
                 {props.text}
             </p>
