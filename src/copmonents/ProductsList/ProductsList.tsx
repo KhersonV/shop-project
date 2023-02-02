8dbe262fdf4172edc1ca6ec03fbcb42d7776d66d
KhersonV <khersonv110990@gmail.com>
2023-02-02 12:06:56 -0600

    Add Props to productslistItem

--- src/copmonents/ProductsList/ProductsList.tsx
+++ src/copmonents/ProductsList/ProductsList.tsx
@@ -12,19 +12,19 @@ const ProductsList = (props: Props) => {
             <Grid container spacing={3}>
                 <Grid item xs={12} sm={6} md={4}>
                     {' '}
-                    <ProductsListItem />
+                    <ProductsListItem title="iPhone 14 pro" description='This iPhone 14 pro' type=' phone' capacity="512 " price={1300}/>
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
                     {' '}
-                    <ProductsListItem />
+                    <ProductsListItem title="iPhone 13 pro" description='This iPhone 13 pro' type=' phone' capacity="256 "  price={1200}/>
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
                     {' '}
-                    <ProductsListItem />
+                    <ProductsListItem title="iPhone 12 pro" description='This iPhone 12 pro'type=' phone' capacity="128 "  price={1100}/>
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
                     {' '}
-                    <ProductsListItem />
+                    <ProductsListItem title="iPhone 11 pro" description='This iPhone 11 pro'type=' phone' capacity="128 "  price={1000}/>
                 </Grid>
             </Grid>
         </>
