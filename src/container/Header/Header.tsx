664fd45e4d375f530000ba8e0a777755c481a902
KhersonV <khersonv110990@gmail.com>
2023-02-01 21:51:48 -0600

    add Logo

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,11 +1,12 @@
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import { Container } from '@mui/system'
+
 import './Header.scss'
+import Logo from 'copmonents/Logo/Logo'
 
 type Props = {}
 const Header = (props: Props) => {
@@ -20,15 +21,10 @@ const Header = (props: Props) => {
                         aria-label="menu"
                         sx={{ mr: 2 }}
                     >
+                        
                         <MenuIcon />
                     </IconButton>
-                    <Typography
-                        variant="h6"
-                        component="div"
-                        sx={{ flexGrow: 1 }}
-                    >
-                        News
-                    </Typography>
+                    <Logo />
                     <Button color="inherit">Login</Button>
                 </Toolbar>
             </Container>
