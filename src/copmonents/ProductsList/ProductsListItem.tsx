32595de76f4fb5939b97a8a6013ba6053b80d404
KhersonV <khersonv110990@gmail.com>
2023-02-12 16:55:28 -0600

    add func +-

--- src/copmonents/ProductsList/ProductsListItem.tsx
+++ src/copmonents/ProductsList/ProductsListItem.tsx
@@ -5,6 +5,7 @@ import {
     CardContent,
     TextField,
 } from '@mui/material'
+import { useState } from 'react'
 import './ProductsListItem.scss'
 
 type Props = {
@@ -24,6 +25,16 @@ const ProductsListItem = ({
     price,
     image,
 }: Props) => {
+    const [count, setCount] = useState<number>(1)
+
+    const onIncrement = () => {
+        setCount(count + 1)
+    }
+
+    const onDecrement = () => {
+        setCount(count - 1)
+    }
+
     return (
         <Card variant="outlined">
             <CardContent>
@@ -36,9 +47,13 @@ const ProductsListItem = ({
                 <div className="product-features">{capacity} GB</div>
                 <div className="product-price">{price}</div>
                 <div className="product-quantity">
-                    <Button variant="outlined">-</Button>
-                    <TextField size="small" value="1" />
-                    <Button variant="outlined">+</Button>
+                    <Button variant="outlined" onClick={() => onDecrement()}>
+                        -
+                    </Button>
+                    <TextField size="small" value={count} />
+                    <Button variant="outlined" onClick={() => onIncrement()}>
+                        +
+                    </Button>
                 </div>
             </CardContent>
             <CardActions className="product-btn-wrap">
